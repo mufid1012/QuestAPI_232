50f65a1255a1a62d17a435e513d71314dec9ce1f
mufid1012 <mufidghibran1012@gmail.com>
2025-12-18 20:47:38 +0700

    Implement initial navigation controller setup for student data application
    - Create `DataSiswaApp` composable function to manage application navigation.
    - Initialize `NavHostController` using `rememberNavController`.
    - Define basic project structure within the `uicontroller` package.

---
+++ app/src/main/java/com/example/localrestapi/view/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,13 @@
+package com.example.localrestapi.view.uicontroller
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun DataSiswaApp(navController: NavHostController = rememberNavController(),
+                 modifier: Modifier){
+
+}
