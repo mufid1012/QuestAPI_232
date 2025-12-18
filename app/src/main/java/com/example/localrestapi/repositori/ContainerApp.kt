6756785e9f479a4c3d638a642b71724c99560160
mufid1012 <mufidghibran1012@gmail.com>
2025-12-18 22:00:05 +0700

    Update API base URL and refactor application class formatting
    - Append trailing slash to `baseurl` in `DefaultContainerApp`.
    - Adjust trailing whitespace in `AplikasiDataSiswa` class definition.

--- app/src/main/java/com/example/localrestapi/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/localrestapi/repositori/ContainerApp.kt
@@ -13,7 +13,7 @@ interface ContainerApp {
     val repositoryDataSiswa: RepositoryDataSiswa
 }
 class DefaultContainerApp: ContainerApp{
-    private val baseurl = "http://10.0.2.2/umyTI"
+    private val baseurl = "http://10.0.2.2/umyTI/"
     val logging = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
     }
