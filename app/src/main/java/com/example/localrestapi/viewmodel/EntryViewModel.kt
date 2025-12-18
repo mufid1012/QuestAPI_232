52c6d49a96e779565b57cc1cc78583a7c42e4aa4
mufid1012 <mufidghibran1012@gmail.com>
2025-12-18 21:08:39 +0700

    Implement `EntryViewModel` for student data entry management
    - Create `EntryViewModel` to handle UI state and data operations for student registration.
    - Implement `uiStateSiswa` using `mutableStateOf` to manage form input and validation status.
    - Add `updateUiState` function to update state and trigger input validation for name, address, and phone fields.
    - Implement `addSiswa` suspend function to persist student data via `RepositoryDataSiswa`.
    - Include basic logging for successful and failed API responses during data insertion.

---
+++ app/src/main/java/com/example/localrestapi/viewmodel/EntryViewModel.kt
@@ -0,0 +1,39 @@
+package com.example.localrestapi.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.localrestapi.modeldata.DetailSiswa
+import com.example.localrestapi.modeldata.UIStateSiswa
+import com.example.localrestapi.modeldata.toDataSiswa
+import com.example.localrestapi.repository.RepositoryDataSiswa
+import retrofit2.Response
+
+class EntryViewModel (private val repositoryDataSiswa: RepositoryDataSiswa):
+    ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa):
+            Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun  updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+    suspend fun addSiswa() {
+        if (validasiInput()) {
+            val sip: Response<Void> = repositoryDataSiswa.postDataSiswa(uiStateSiswa.detailSiswa.toDataSiswa())
+            if (sip.isSuccessful) {
+                println("Sukses Tambah Data: ${sip.message()}")
+            }else{
+                println("Gagal Tambah Data: ${sip.errorBody()}")
+            }
+        }
+    }
+}
