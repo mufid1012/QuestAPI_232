201dc0ef1df447b9bd32c78e5ea7b7412a491005
mufid1012 <mufidghibran1012@gmail.com>
2025-12-18 20:48:30 +0700

    Implement navigation destination for the home screen
    - Create `DestinasiHome` object implementing the `DestinasiNavigasi` interface.
    - Define the navigation route as "home" and assign the application name as the title resource.

---
+++ app/src/main/java/com/example/localrestapi/view/route/DestinasniHome.kt
@@ -0,0 +1,9 @@
+package com.example.localrestapi.view.route
+
+
+import com.example.localrestapi.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
