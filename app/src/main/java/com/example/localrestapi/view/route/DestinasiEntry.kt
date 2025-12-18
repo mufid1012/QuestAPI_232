db51c79a86164b70e44fa9998870c2d667110e29
mufid1012 <mufidghibran1012@gmail.com>
2025-12-18 21:08:35 +0700

    Implement `DestinasiEntry` navigation object
    - Define `DestinasiEntry` implementing `DestinasiNavigasi`.
    - Set navigation route to `item_entry` and assign the title resource from `R.string.entry_siswa`.

--- app/src/main/java/com/example/localrestapi/view/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/localrestapi/view/route/DestinasiEntry.kt
@@ -1,2 +1,8 @@
 package com.example.localrestapi.view.route
 
+import com.example.localrestapi.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
