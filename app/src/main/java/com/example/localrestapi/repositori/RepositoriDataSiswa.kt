431fa94718edd2a4a0f978694e746954aeb7a09b
mufid1012 <mufidghibran1012@gmail.com>
2025-12-18 17:05:59 +0700

    Refactor repository package and update imports
    - Rename package from `repositori` to `repository`.
    - Update imports to reflect new package structure for models and services.
    - Clean up code formatting and remove unnecessary whitespace in `RepositoriDataSiswa.kt`.

--- app/src/main/java/com/example/localrestapi/repositori/RepositoriDataSiswa.kt
+++ app/src/main/java/com/example/localrestapi/repositori/RepositoriDataSiswa.kt
@@ -1,18 +1,16 @@
-package com.example.localrestapi.repositori
+package com.example.localrestapi.repository
 
-import com.example.localrestapi.apiservice.ServiceApiSiswa
-import com.example.localrestapi.modeldata.DataSiswa
+import com.example.localrestapi.model.DataSiswa
+import com.example.localrestapi.service.ServiceApiSiswa
 
 interface RepositoryDataSiswa {
-    suspend fun getDataSiswa() : List<DataSiswa>
-
-    suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
+    suspend fun getDataSiswa(): List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>
 }
-
-
 class JaringanRepositoryDataSiswa(
     private val serviceApiSiswa: ServiceApiSiswa
 ): RepositoryDataSiswa{
-    override suspend fun getDataSiswa() : List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
     override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+
 }
