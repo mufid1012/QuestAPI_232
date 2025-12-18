ad59f46afb1ce0ec40d67be3ff680f1042e3f2ab
mufid1012 <mufidghibran1012@gmail.com>
2025-12-18 21:08:31 +0700

    Rename `DetailSiswa.toSiswa()` to `DetailSiswa.toDataSiswa()`
    - Rename the extension function `toSiswa` to `toDataSiswa` in `DataSiswa.kt` for improved naming consistency.

--- app/src/main/java/com/example/localrestapi/modeldata/DataSiswa.kt
+++ app/src/main/java/com/example/localrestapi/modeldata/DataSiswa.kt
@@ -23,7 +23,7 @@ data class DetailSiswa(
     val telpon: String = ""
 )
 
-fun DetailSiswa.toSiswa(): DataSiswa = DataSiswa(
+fun DetailSiswa.toDataSiswa(): DataSiswa = DataSiswa(
     id = id,
     nama = nama,
     alamat = alamat,
