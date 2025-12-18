962650ed424ec7a0e60ade4cfcd93423e65f5754
mufid1012 <mufidghibran1012@gmail.com>
2025-12-18 21:09:01 +0700

    Update `DataSiswa` import to reflect package change
    - Update import path for `DataSiswa` model from `model` to `modeldata`.

--- app/src/main/java/com/example/localrestapi/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/localrestapi/apiservice/ServiceApiSiswa.kt
@@ -1,6 +1,6 @@
 package com.example.localrestapi.service
 
-import com.example.localrestapi.model.DataSiswa
+import com.example.localrestapi.modeldata.DataSiswa
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
