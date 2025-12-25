fd3a55e0b1179f8548e6e5a1dee96f25b823f012
mufid1012 <mufidghibran1012@gmail.com>
2025-12-25 12:57:16 +0700

    Implement ViewModel initializers for detail and edit screens in `PenyediaViewModel`
    - Add `DetailViewModel` and `EditViewModel` to the `PenyediaViewModel` factory.
    - Integrate `createSavedStateHandle()` into the initializers for detail and edit view models to support state persistence.
    - Refactor the `aplikasiDataSiswa` helper function for improved readability.
    - Clean up code formatting and spacing within the `PenyediaViewModel` object.

--- app/src/main/java/com/example/localrestapi/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/localrestapi/viewmodel/provider/PenyediaViewModel.kt
@@ -1,23 +1,49 @@
 package com.example.localrestapi.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.localrestapi.repository.AplikasiDataSiswa
+import com.example.localrestapi.viewmodel.DetailViewModel
+import com.example.localrestapi.viewmodel.EditViewModel
 import com.example.localrestapi.viewmodel.EntryViewModel
 import com.example.localrestapi.viewmodel.HomeViewModel
 
-fun CreationExtras.aplikasiDataSiswa() : AplikasiDataSiswa = (
-        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
-                AplikasiDataSiswa)
+/* Ambil instance Application */
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
+    this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]
+            as AplikasiDataSiswa
+
 object PenyediaViewModel {
-    val Factory = viewModelFactory  {
+
+    val Factory = viewModelFactory {
+
         initializer {
-            HomeViewModel(aplikasiDataSiswa().container.repositoryDataSiswa)
+            HomeViewModel(
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
         }
+
         initializer {
-            EntryViewModel(aplikasiDataSiswa().container.repositoryDataSiswa)
+            EntryViewModel(
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
+
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
+
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
         }
     }
 }
