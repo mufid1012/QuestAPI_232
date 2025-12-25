be1b479ac7dfed2c4edd06fb021c5caa01f3364f
mufid1012 <mufidghibran1012@gmail.com>
2025-12-25 10:59:08 +0700

    Update repository interface and implementation to support updating student data
    - Add `editSatuSiswa` method to `RepositoryDataSiswa` interface for updating a single student's data.
    - Implement `editSatuSiswa` in `JaringanRepositoryDataSiswa` using the API service.

--- app/src/main/java/com/example/localrestapi/repositori/RepositoriDataSiswa.kt
+++ app/src/main/java/com/example/localrestapi/repositori/RepositoriDataSiswa.kt
@@ -7,6 +7,7 @@ interface RepositoryDataSiswa {
     suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>
     suspend fun getSatuSiswa(id:Int) : DataSiswa
+    suspend fun editSatuSiswa(id:Int,dataSiswa: DataSiswa):retrofit2.Response<Void>
 }
 class JaringanRepositoryDataSiswa(
     private val serviceApiSiswa: ServiceApiSiswa
@@ -14,5 +15,7 @@ class JaringanRepositoryDataSiswa(
     override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
     override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
     override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
+    override suspend fun editSatuSiswa(id: Int,dataSiswa: DataSiswa):retrofit2.Response<Void> =
+        serviceApiSiswa.editSatuSiswa(id, dataSiswa)
 
 }
