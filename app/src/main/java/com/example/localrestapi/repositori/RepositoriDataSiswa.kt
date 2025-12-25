50186e8a6e9748c17b42ae7f2b2730e2f45fee16
mufid1012 <mufidghibran1012@gmail.com>
2025-12-25 10:58:29 +0700

    Add single student retrieval functionality to repository
    - Update `RepositoryDataSiswa` interface to include `getSatuSiswa` for fetching a single student by ID.
    - Implement `getSatuSiswa` in `JaringanRepositoryDataSiswa` using the `serviceApiSiswa` instance.

--- app/src/main/java/com/example/localrestapi/repositori/RepositoriDataSiswa.kt
+++ app/src/main/java/com/example/localrestapi/repositori/RepositoriDataSiswa.kt
@@ -6,11 +6,13 @@ import com.example.localrestapi.service.ServiceApiSiswa
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>
+    suspend fun getSatuSiswa(id:Int) : DataSiswa
 }
 class JaringanRepositoryDataSiswa(
     private val serviceApiSiswa: ServiceApiSiswa
 ): RepositoryDataSiswa{
     override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
     override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
 
 }
